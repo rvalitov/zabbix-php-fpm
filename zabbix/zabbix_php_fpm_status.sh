a35cb432b9ca7651cb973c3b3a1e89ae1ef0b82c
Ramil Valitov <ramilvalitov@gmail.com>
2019-11-26 18:26:21 +0300

    [add] info how to use this script

--- zabbix/zabbix_php_fpm_status.sh
+++ zabbix/zabbix_php_fpm_status.sh
@@ -18,9 +18,10 @@ fi
 
 if [[ -z $1 ]] || [[ -z $2 ]]; then
 	echo "No input data specified"
-	echo "Usage: $0 socket status"
+	echo "Usage: $0 php-path status"
 	echo "where:"
-	echo "socket - path to socket file, for example, /var/lib/php7.3-fpm/web1.sock"
+	echo "php-path - path to socket file, for example, /var/lib/php7.3-fpm/web1.sock"
+	echo "or IP and port of the PHP-FPM, for example, 127.0.0.1:9000"
 	echo "status - path configured in pm.status of PHP-FPM"
 	exit 1
 fi
