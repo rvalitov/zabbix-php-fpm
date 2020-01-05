9219d408f7e22d5aa1e2dc6fe26bf5a5af28004a
Ramil Valitov <ramilvalitov@gmail.com>
2020-01-05 06:22:35 +0300

    [add] cache for ondemand pm and check status of pools during discovery

--- zabbix/zabbix_php_fpm_discovery.sh
+++ zabbix/zabbix_php_fpm_discovery.sh
@@ -10,6 +10,9 @@ S_SORT=`type -P sort`
 S_HEAD=`type -P head`
 S_LSOF=`type -P lsof`
 S_JQ=`type -P jq`
+S_DIRNAME=`type -P dirname`
+S_CAT=`type -P cat`
+S_BASH=`type -P bash`
 
 if [[ ! -f $S_PS ]]; then
 	echo "Utility 'ps' not found. Please, install it first."
@@ -39,12 +42,21 @@ if [[ ! -f $S_JQ ]]; then
 	echo "Utility 'jq' not found. Please, install it first."
 	exit 1
 fi
-
-DEBUG_MODE=""
-if [[ ! -z $1 ]] && [[ $1 == "debug" ]]; then
-    DEBUG_MODE="1"
-    echo "Debug mode enabled"
+if [[ ! -f ${S_DIRNAME} ]]; then
+	echo "Utility 'dirname' not found. Please, install it first."
+	exit 1
 fi
+if [[ ! -f ${S_CAT} ]]; then
+	echo "Utility 'cat' not found. Please, install it first."
+	exit 1
+fi
+if [[ ! -f ${S_BASH} ]]; then
+	echo "Utility 'bash' not found. Please, install it first."
+	exit 1
+fi
+
+STATUS_PATH="/php-fpm-status"
+DEBUG_MODE=""
 
 # Prints a string on screen. Works only if debug mode is enabled.
 function PrintDebug(){
@@ -52,6 +64,140 @@ function PrintDebug(){
 	    echo $1
 	fi
 }
+
+# Encodes input data to JSON and saves it to result string
+# Input arguments:
+# - pool name
+# - pool socket
+# Function returns 1 if all OK, and 0 otherwise.
+function EncodeToJson(){
+    POOL_NAME=$1
+    POOL_SOCKET=$2
+    if [[ -z ${POOL_NAME} ]] || [[ -z ${POOL_SOCKET} ]]; then
+        return 0
+    fi
+
+    JSON_POOL=`echo -n "$POOL_NAME" | ${S_JQ} -aR .`
+    JSON_SOCKET=`echo -n "$POOL_SOCKET" | ${S_JQ} -aR .`
+    if [[ ${POOL_FIRST} == 1 ]]; then
+        RESULT_DATA="$RESULT_DATA,"
+    fi
+    RESULT_DATA="$RESULT_DATA{\"{#POOLNAME}\":$JSON_POOL,\"{#POOLSOCKET}\":$JSON_SOCKET}"
+    POOL_FIRST=1
+    return 1
+}
+
+# Checks if selected pool is in cache.
+# Input arguments:
+# - pool name
+# - pool socket
+# Function returns 1 if the pool is in cache, and 0 otherwise.
+function IsInCache(){
+    SEARCH_NAME=$1
+    SEARCH_SOCKET=$2
+    if [[ -z ${SEARCH_NAME} ]] || [[ -z ${SEARCH_SOCKET} ]]; then
+        return 0
+    fi
+    for CACHE_ITEM in "${NEW_CACHE[@]}"
+    do
+        ITEM_NAME=`echo "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
+        ITEM_SOCKET=`echo "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
+        if [[ ${ITEM_NAME} == ${SEARCH_NAME} ]] && [[ ${ITEM_SOCKET} == ${SEARCH_SOCKET} ]]; then
+            return 1
+        fi
+    done
+    return 0
+}
+
+# Validates the specified pool by getting its status and working with cache.
+# Pass two arguments: pool name and pool socket
+# Function returns:
+# 0 if the pool is invalid
+# 1 if the pool is OK and is ondemand and is not in cache
+# 2 if the pool is OK and is ondemand and is in cache
+# 3 if the pool is OK and is not ondemand and is not in cache
+function ProcessPool(){
+    POOL_NAME=$1
+    POOL_SOCKET=$2
+    if [[ -z ${POOL_NAME} ]] || [[ -z ${POOL_SOCKET} ]]; then
+        return 0
+    fi
+
+    IsInCache ${POOL_NAME} ${POOL_SOCKET}
+    FOUND=$?
+    if [[ ${FOUND} == 1 ]]; then
+        return 2
+    fi
+
+    STATUS_JSON=`${S_BASH} ${STATUS_SCRIPT} ${POOL_SOCKET} ${STATUS_PATH}`
+    EXIT_CODE=$?
+    if [[ ${EXIT_CODE} == 0 ]]; then
+        # The exit code is OK, let's check the JSON data
+        # JSON data example:
+        # {"pool":"www2","process manager":"ondemand","start time":1578181845,"start since":117,"accepted conn":3,"listen queue":0,"max listen queue":0,"listen queue len":0,"idle processes":0,"active processes":1,"total processes":1,"max active processes":1,"max children reached":0,"slow requests":0}
+        # We use basic regular expression here, i.e. we need to use \+ and not escape { and }
+        if [[ ! -z `echo ${STATUS_JSON} | ${S_GREP} -G '^{.*\"pool\":\".\+\".*,\"process manager\":\".\+\".*}$'` ]]; then
+            PrintDebug "Status data for pool $POOL_NAME, socket $POOL_SOCKET, status path $STATUS_PATH is valid"
+            # Checking if we have ondemand pool
+            if [[ ! -z `echo ${STATUS_JSON} | ${S_GREP} -F '"process manager":"ondemand"'` ]]; then
+                PrintDebug "Detected pool's process manager is ondemand, it needs to be cached"
+                NEW_CACHE+=("$POOL_NAME $POOL_SOCKET")
+                return 1
+            fi
+            PrintDebug "Detected pool's process manager is NOT ondemand, it will not be cached"
+            return 3
+        fi
+
+        PrintDebug "Failed to validate status data for pool $POOL_NAME, socket $POOL_SOCKET, status path $STATUS_PATH"
+        if [[ ! -z ${STATUS_JSON} ]]; then
+            PrintDebug "Status script returned: $STATUS_JSON"
+        fi
+        return 0
+    fi
+    PrintDebug "Failed to get status for pool $POOL_NAME, socket $POOL_SOCKET, status path $STATUS_PATH"
+    if [[ ! -z ${STATUS_JSON} ]]; then
+        PrintDebug "Status script returned: $STATUS_JSON"
+    fi
+    return 0
+}
+
+for ARG in "$@"; do
+    if [[ ${ARG} == "debug" ]]; then
+        DEBUG_MODE="1"
+        echo "Debug mode enabled"
+    elif [[ ${ARG} == /* ]]; then
+        STATUS_PATH=${ARG}
+        PrintDebug "Argument $ARG is interpreted as status path"
+    else
+        PrintDebug "Argument $ARG is unknown and skipped"
+    fi
+done
+PrintDebug "Status path to be used: $STATUS_PATH"
+
+LOCAL_DIR=`${S_DIRNAME} $0`
+CACHE_FILE="$LOCAL_DIR/php_fpm.cache"
+STATUS_SCRIPT="$LOCAL_DIR/zabbix_php_fpm_status.sh"
+PrintDebug "Local directory is $LOCAL_DIR"
+if [[ ! -f ${STATUS_SCRIPT} ]]; then
+    echo "Helper script $STATUS_SCRIPT not found"
+    exit 1
+fi
+if [[ ! -r ${STATUS_SCRIPT} ]]; then
+    echo "Helper script $STATUS_SCRIPT is not readable"
+    exit 1
+fi
+PrintDebug "Helper script $STATUS_SCRIPT is reachable"
+
+# Loading cached data for ondemand pools.
+# The cache file consists of lines, each line contains pool name, then space, then socket (or TCP info)
+CACHE=()
+NEW_CACHE=()
+if [[ -r ${CACHE_FILE} ]]; then
+    PrintDebug "Reading cache file $CACHE_FILE..."
+    mapfile -t CACHE < <( ${S_CAT} ${CACHE_FILE} )
+else
+    PrintDebug "Cache file $CACHE_FILE not found, skipping..."
+fi
 
 mapfile -t PS_LIST < <( $S_PS ax | $S_GREP -F "php-fpm: pool " | $S_GREP -F -v "grep" )
 POOL_LIST=`printf '%s\n' "${PS_LIST[@]}" | $S_AWK '{print $NF}' | $S_SORT -u`
@@ -89,8 +235,15 @@ do
                         if [[ $POOL_TYPE == "unix" ]]; then
                             #We have a socket here, test if it's actually a socket:
                             if [[ -S $POOL_SOCKET ]]; then
-                                FOUND_POOL="1"
-                                PrintDebug "Success: found socket $POOL_SOCKET"
+                                PrintDebug "Found socket $POOL_SOCKET"
+                                ProcessPool ${line} ${POOL_SOCKET}
+                                POOL_STATUS=$?
+                                if [[ ${POOL_STATUS} > 0 ]]; then
+                                    FOUND_POOL="1"
+                                    PrintDebug "Success: socket $POOL_SOCKET returned valid status data"
+                                else
+                                    PrintDebug "Error: socket $POOL_SOCKET didn't return valid data"
+                                fi
                             else
                                 PrintDebug "Error: specified socket $POOL_SOCKET is not valid"
                             fi
@@ -104,8 +257,15 @@ do
                                     #Check and replace * to localhost if it's found. Asterisk means that the PHP listens on
                                     #all interfaces.
                                     POOL_SOCKET=`echo -n ${POOL_SOCKET/*:/localhost:}`
-                                    FOUND_POOL="1"
-                                    PrintDebug "Success: found TCP connection $POOL_SOCKET"
+                                    PrintDebug "Found TCP connection $POOL_SOCKET"
+                                    ProcessPool ${line} ${POOL_SOCKET}
+                                    POOL_STATUS=$?
+                                    if [[ ${POOL_STATUS} > 0 ]]; then
+                                        FOUND_POOL="1"
+                                        PrintDebug "Success: TCP connection $POOL_SOCKET returned valid status data"
+                                    else
+                                        PrintDebug "Error: TCP connection $POOL_SOCKET didn't return valid data"
+                                    fi
                                 else
                                     PrintDebug "Warning: expected connection state must be LISTEN, but it was not detected"
                                 fi
@@ -126,14 +286,8 @@ do
             fi
         done <<< "$POOL_PARAMS_LIST"
 
-        if [[ ! -z $FOUND_POOL ]]; then
-            JSON_POOL=`echo -n "$line" | $S_JQ -aR .`
-            JSON_SOCKET=`echo -n "$POOL_SOCKET" | $S_JQ -aR .`
-            if [[ $POOL_FIRST == 1 ]]; then
-                RESULT_DATA="$RESULT_DATA,"
-            fi
-            RESULT_DATA="$RESULT_DATA{\"{#POOLNAME}\":$JSON_POOL,\"{#POOLSOCKET}\":$JSON_SOCKET}"
-            POOL_FIRST=1
+        if [[ ! -z ${FOUND_POOL} ]]; then
+            EncodeToJson ${line} ${POOL_SOCKET}
         else
             PrintDebug "Error: failed to discover information for pool $line"
         fi
@@ -141,6 +295,23 @@ do
         PrintDebug "Error: failed to find PID for pool $line"
     fi
 done <<< "$POOL_LIST"
+
+PrintDebug "Processing pools from old cache..."
+for CACHE_ITEM in "${CACHE[@]}"
+do
+    ITEM_NAME=`echo "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
+    ITEM_SOCKET=`echo "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
+    ProcessPool ${ITEM_NAME} ${ITEM_SOCKET}
+    POOL_STATUS=$?
+    if [[ ${POOL_STATUS} == "1" ]]; then
+        # This is a new pool and we must add it
+        EncodeToJson ${ITEM_NAME} ${ITEM_SOCKET}
+    fi
+done
+
+PrintDebug "Saving new cache file $CACHE_FILE..."
+printf "%s\n" "${NEW_CACHE[@]}" > ${CACHE_FILE}
+
 RESULT_DATA="$RESULT_DATA]}"
 PrintDebug "Resulting JSON data for Zabbix:"
 echo -n $RESULT_DATA
