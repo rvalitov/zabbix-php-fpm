98cab3a90bdcdd7f7987162a03705fd2a98ec5dd
Ramil Valitov <ramilvalitov@gmail.com>
2020-01-05 06:29:09 +0300

    [fix] improved detection of some commands

--- zabbix/zabbix_php_fpm_discovery.sh
+++ zabbix/zabbix_php_fpm_discovery.sh
@@ -13,45 +13,51 @@ S_JQ=`type -P jq`
 S_DIRNAME=`type -P dirname`
 S_CAT=`type -P cat`
 S_BASH=`type -P bash`
+S_ECHO=`type -P echo`
+S_PRINTF=`type -P printf`
 
 if [[ ! -f $S_PS ]]; then
-	echo "Utility 'ps' not found. Please, install it first."
+	${S_ECHO} "Utility 'ps' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_GREP ]]; then
-	echo "Utility 'grep' not found. Please, install it first."
+	${S_ECHO} "Utility 'grep' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_AWK ]]; then
-	echo "Utility 'awk' not found. Please, install it first."
+	${S_ECHO} "Utility 'awk' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_SORT ]]; then
-	echo "Utility 'sort' not found. Please, install it first."
+	${S_ECHO} "Utility 'sort' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_HEAD ]]; then
-	echo "Utility 'head' not found. Please, install it first."
+	${S_ECHO} "Utility 'head' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_LSOF ]]; then
-	echo "Utility 'lsof' not found. Please, install it first."
+	${S_ECHO} "Utility 'lsof' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f $S_JQ ]]; then
-	echo "Utility 'jq' not found. Please, install it first."
+	${S_ECHO} "Utility 'jq' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f ${S_DIRNAME} ]]; then
-	echo "Utility 'dirname' not found. Please, install it first."
+	${S_ECHO} "Utility 'dirname' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f ${S_CAT} ]]; then
-	echo "Utility 'cat' not found. Please, install it first."
+	${S_ECHO} "Utility 'cat' not found. Please, install it first."
 	exit 1
 fi
 if [[ ! -f ${S_BASH} ]]; then
-	echo "Utility 'bash' not found. Please, install it first."
+	${S_ECHO} "Utility 'bash' not found. Please, install it first."
+	exit 1
+fi
+if [[ ! -f ${S_PRINTF} ]]; then
+	${S_ECHO} "Utility 'printf' not found. Please, install it first."
 	exit 1
 fi
 
@@ -61,7 +67,7 @@ DEBUG_MODE=""
 # Prints a string on screen. Works only if debug mode is enabled.
 function PrintDebug(){
 	if [[ ! -z $DEBUG_MODE ]] && [[ ! -z $1 ]]; then
-	    echo $1
+	    ${S_ECHO} $1
 	fi
 }
 
@@ -77,8 +83,8 @@ function EncodeToJson(){
         return 0
     fi
 
-    JSON_POOL=`echo -n "$POOL_NAME" | ${S_JQ} -aR .`
-    JSON_SOCKET=`echo -n "$POOL_SOCKET" | ${S_JQ} -aR .`
+    JSON_POOL=`${S_ECHO} -n "$POOL_NAME" | ${S_JQ} -aR .`
+    JSON_SOCKET=`${S_ECHO} -n "$POOL_SOCKET" | ${S_JQ} -aR .`
     if [[ ${POOL_FIRST} == 1 ]]; then
         RESULT_DATA="$RESULT_DATA,"
     fi
@@ -100,8 +106,8 @@ function IsInCache(){
     fi
     for CACHE_ITEM in "${NEW_CACHE[@]}"
     do
-        ITEM_NAME=`echo "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
-        ITEM_SOCKET=`echo "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
+        ITEM_NAME=`${S_ECHO} "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
+        ITEM_SOCKET=`${S_ECHO} "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
         if [[ ${ITEM_NAME} == ${SEARCH_NAME} ]] && [[ ${ITEM_SOCKET} == ${SEARCH_SOCKET} ]]; then
             return 1
         fi
@@ -136,10 +142,10 @@ function ProcessPool(){
         # JSON data example:
         # {"pool":"www2","process manager":"ondemand","start time":1578181845,"start since":117,"accepted conn":3,"listen queue":0,"max listen queue":0,"listen queue len":0,"idle processes":0,"active processes":1,"total processes":1,"max active processes":1,"max children reached":0,"slow requests":0}
         # We use basic regular expression here, i.e. we need to use \+ and not escape { and }
-        if [[ ! -z `echo ${STATUS_JSON} | ${S_GREP} -G '^{.*\"pool\":\".\+\".*,\"process manager\":\".\+\".*}$'` ]]; then
+        if [[ ! -z `${S_ECHO} ${STATUS_JSON} | ${S_GREP} -G '^{.*\"pool\":\".\+\".*,\"process manager\":\".\+\".*}$'` ]]; then
             PrintDebug "Status data for pool $POOL_NAME, socket $POOL_SOCKET, status path $STATUS_PATH is valid"
             # Checking if we have ondemand pool
-            if [[ ! -z `echo ${STATUS_JSON} | ${S_GREP} -F '"process manager":"ondemand"'` ]]; then
+            if [[ ! -z `${S_ECHO} ${STATUS_JSON} | ${S_GREP} -F '"process manager":"ondemand"'` ]]; then
                 PrintDebug "Detected pool's process manager is ondemand, it needs to be cached"
                 NEW_CACHE+=("$POOL_NAME $POOL_SOCKET")
                 return 1
@@ -164,7 +170,7 @@ function ProcessPool(){
 for ARG in "$@"; do
     if [[ ${ARG} == "debug" ]]; then
         DEBUG_MODE="1"
-        echo "Debug mode enabled"
+        ${S_ECHO} "Debug mode enabled"
     elif [[ ${ARG} == /* ]]; then
         STATUS_PATH=${ARG}
         PrintDebug "Argument $ARG is interpreted as status path"
@@ -179,11 +185,11 @@ CACHE_FILE="$LOCAL_DIR/php_fpm.cache"
 STATUS_SCRIPT="$LOCAL_DIR/zabbix_php_fpm_status.sh"
 PrintDebug "Local directory is $LOCAL_DIR"
 if [[ ! -f ${STATUS_SCRIPT} ]]; then
-    echo "Helper script $STATUS_SCRIPT not found"
+    ${S_ECHO} "Helper script $STATUS_SCRIPT not found"
     exit 1
 fi
 if [[ ! -r ${STATUS_SCRIPT} ]]; then
-    echo "Helper script $STATUS_SCRIPT is not readable"
+    ${S_ECHO} "Helper script $STATUS_SCRIPT is not readable"
     exit 1
 fi
 PrintDebug "Helper script $STATUS_SCRIPT is reachable"
@@ -200,13 +206,13 @@ else
 fi
 
 mapfile -t PS_LIST < <( $S_PS ax | $S_GREP -F "php-fpm: pool " | $S_GREP -F -v "grep" )
-POOL_LIST=`printf '%s\n' "${PS_LIST[@]}" | $S_AWK '{print $NF}' | $S_SORT -u`
+POOL_LIST=`${S_PRINTF} '%s\n' "${PS_LIST[@]}" | $S_AWK '{print $NF}' | $S_SORT -u`
 POOL_FIRST=0
 #We store the resulting JSON data for Zabbix in the following var:
 RESULT_DATA="{\"data\":["
 while IFS= read -r line
 do
-    POOL_PID=`printf '%s\n' "${PS_LIST[@]}" | $S_GREP -F -w "php-fpm: pool $line" | $S_HEAD -1 | $S_AWK '{print $1}'`
+    POOL_PID=`${S_PRINTF} '%s\n' "${PS_LIST[@]}" | $S_GREP -F -w "php-fpm: pool $line" | $S_HEAD -1 | $S_AWK '{print $1}'`
     if [[ ! -z $POOL_PID ]]; then
         #We search for socket or IP address and port
         #Socket example:
@@ -229,8 +235,8 @@ do
             if [[ ! -z $pool ]]; then
                 if [[ -z $FOUND_POOL ]]; then
                     PrintDebug "Checking process: $pool"
-                    POOL_TYPE=`echo "${pool}" | $S_AWK '{print $5}'`
-                    POOL_SOCKET=`echo "${pool}" | $S_AWK '{print $9}'`
+                    POOL_TYPE=`${S_ECHO} "${pool}" | $S_AWK '{print $5}'`
+                    POOL_SOCKET=`${S_ECHO} "${pool}" | $S_AWK '{print $9}'`
                     if [[ ! -z $POOL_TYPE ]] && [[ ! -z $POOL_SOCKET ]]; then
                         if [[ $POOL_TYPE == "unix" ]]; then
                             #We have a socket here, test if it's actually a socket:
@@ -249,14 +255,14 @@ do
                             fi
                         elif [[ $POOL_TYPE == "IPv4" ]] || [[ $POOL_TYPE == "IPv6" ]]; then
                             #We have a TCP connection here, check it:
-                            CONNECTION_TYPE=`echo "${pool}" | $S_AWK '{print $8}'`
+                            CONNECTION_TYPE=`${S_ECHO} "${pool}" | $S_AWK '{print $8}'`
                             if [[ $CONNECTION_TYPE == "TCP" ]]; then
                                 #The connection must have state LISTEN:
-                                LISTEN=`echo ${pool} | $S_GREP -F -w "(LISTEN)"`
+                                LISTEN=`${S_ECHO} ${pool} | $S_GREP -F -w "(LISTEN)"`
                                 if [[ ! -z $LISTEN ]]; then
                                     #Check and replace * to localhost if it's found. Asterisk means that the PHP listens on
                                     #all interfaces.
-                                    POOL_SOCKET=`echo -n ${POOL_SOCKET/*:/localhost:}`
+                                    POOL_SOCKET=`${S_ECHO} -n ${POOL_SOCKET/*:/localhost:}`
                                     PrintDebug "Found TCP connection $POOL_SOCKET"
                                     ProcessPool ${line} ${POOL_SOCKET}
                                     POOL_STATUS=$?
@@ -299,8 +305,8 @@ done <<< "$POOL_LIST"
 PrintDebug "Processing pools from old cache..."
 for CACHE_ITEM in "${CACHE[@]}"
 do
-    ITEM_NAME=`echo "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
-    ITEM_SOCKET=`echo "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
+    ITEM_NAME=`${S_ECHO} "$CACHE_ITEM" | ${S_AWK} '{print $1}'`
+    ITEM_SOCKET=`${S_ECHO} "$CACHE_ITEM" | ${S_AWK} '{print $2}'`
     ProcessPool ${ITEM_NAME} ${ITEM_SOCKET}
     POOL_STATUS=$?
     if [[ ${POOL_STATUS} == "1" ]]; then
@@ -310,8 +316,8 @@ do
 done
 
 PrintDebug "Saving new cache file $CACHE_FILE..."
-printf "%s\n" "${NEW_CACHE[@]}" > ${CACHE_FILE}
+${S_PRINTF} "%s\n" "${NEW_CACHE[@]}" > ${CACHE_FILE}
 
 RESULT_DATA="$RESULT_DATA]}"
 PrintDebug "Resulting JSON data for Zabbix:"
-echo -n $RESULT_DATA
+${S_ECHO} -n $RESULT_DATA
